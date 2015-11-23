d60e3691b2afe768b10b5f933009fcc664abb448
yusong-shen <yusongtju@gmail.com>
2015-11-23 14:32:42 -0600

    Descripete the Ford Fulkerson algorithm, ready to implement.
    Todo : fill out all the TODO content

--- src/MaxFlow.java
+++ src/MaxFlow.java
@@ -8,10 +8,36 @@
  *
  */
 public class MaxFlow {
-	
+	/*
+	 * value of max flow
+	 */
+	private double maxflow;
+
 	public MaxFlow(DirectedGraph G, int s, int t){
+		// change original graph to residual graph
+		DirectedGraph resiualG = makeResidualGraph(G);
+		maxflow = 0.0;
+		// check whether there is a augmenting path from s to t
+		// it equals to whether there is a direct path from s to t
+		// which can be solved by BFS
+		while (hasAugmentingPath(G, s, t)){
+			//TODO : modify the flow network
+			// first, compute bottleneck capacity
+			// then, augment flow
+		}
+	}
+	
+	private boolean hasAugmentingPath(DirectedGraph g, int s, int t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public DirectedGraph makeResidualGraph(DirectedGraph G){
+		//TODO 
+		return G;
 		
 	}
+	
 	/**
 	 * @param args
 	 */
