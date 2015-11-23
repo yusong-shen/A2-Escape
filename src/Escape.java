03f4aa4036f4302fc1835f9d16a610fc2476bd53
yusong-shen <yusongtju@gmail.com>
2015-11-22 23:12:13 -0600

    initial commit

---
+++ src/Escape.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+/**
+ * Given graph G, a subset of nodes X and S, this algorithm
+ * will return "YES" if a valid evacuation path set exists,"NO" otherwise.
+ * See more explanation on pdf instruction.
+ * <p>
+ * It can be reduced to a Maxflow problem.
+ * @author yusong
+ *
+ */
+public class Escape {
+
+	/**
+	 * Escape input.txt
+	 * --> "YES" (or "NO")
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String file = args[0];
+		try{
+			List<String> lines = Files.readAllLines(Paths.get(file));
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
